72385d710c98f527d0c2e226c4f0367a7c8c971e
M.Meyland <125752061+MeylandMan@users.noreply.github.com>
2026-01-25 02:45:41 +0100

    Tested the pymysql connection

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import customtkinter as ctk
 import pymysql
 
-print("Hello world")
+connection = None
+
+try:
+    connection = pymysql.connect(
+        host= 'localhost',
+        port= 3306,
+        user= 'root',
+        password= '',
+        database='mcrm'
+    )
+
+    if connection:
+        print("Connected to the database !")
+except pymysql.Error as error:
+    print("Error while connecting to MySQL", error)
+finally:
+    if connection:
+        connection.close()
