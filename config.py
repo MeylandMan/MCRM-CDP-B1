590349218cce075b2ec49dea206a1995ab8932e7
M.Meyland <125752061+MeylandMan@users.noreply.github.com>
2026-01-25 02:49:12 +0100

    Created the function I'll use for getting connection to the db

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import pymysql
 
 DB_CONFIG = {
     'host': os.getenv('MCRM_DB_HOST') or 'localhost',
@@ -7,3 +8,19 @@ DB_CONFIG = {
     'password': os.getenv('MCRM_DB_PASSWORD') or '',
     'database': os.getenv('MCRM_DB_DATABASE') or 'mcrm'
 }
+
+def get_connection():
+    connection = None
+
+    try:
+        connection = pymysql.connect(
+            host=DB_CONFIG['host'],
+            port=DB_CONFIG['port'],
+            user=DB_CONFIG['user'],
+            password=DB_CONFIG['password'],
+            database=DB_CONFIG['database']
+        )
+
+        return connection
+    except pymysql.Error as err:
+        print("Error while connecting to MySQL", err)
